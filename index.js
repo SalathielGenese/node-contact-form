e2f2b74e1c29adf8d380d72f04fb8de3c5fcf2cd
SalathielGenese <salathielgenese@gmail.com>
2019-02-14 15:11:29 +0100

    FIX parse FormData

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fs = require( 'fs' );
 
 const app = express();
 app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
 
 app.post( '/api/contact/email', async ( request, response ) =>
 {
